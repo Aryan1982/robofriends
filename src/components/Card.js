bb5101ccce0d5b0ce8fab9b42924b2ce46f521f0
Aryan1982 <ap1982o3@gmail.com>
2022-08-20 13:30:16 +0530

    done

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import { Link } from 'react-router-dom';
 
-import { BrowserRouter as Router, Switch, 
+import { HashRouter as Router, Switch, 
      } from "react-router-dom";
 
 
--- src/components/Information.js
+++ src/components/Information.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import Info from "./Info.jsx"
-import Info3 from "./Info3"
-import Info2 from "./info2.jsx"
-import Info4 from "./Info4"
-import { robots } from '../containers/robots';
 import BlogDetails from "./params"
 
 
@@ -19,21 +14,9 @@ return(
     
         <Router>
                 <Switch>
-                    {/*<Route path={`/Info`}>
-                    <Info/>
-                </Route> 
-                <Route path={`/Info3`}>
-                    <Info3/>
-                </Route>  
-                <Route path={`/Info2`}>
-                    <Info2/>
-                </Route> 
-                <Route path={`/Info4`}>
-                    <Info4/>
-                </Route> */}
-                <Route path={`/Blogs/:id`}>
-                    <BlogDetails/>
-                </Route>  
+                    <Route path={`/Blogs/:id`}>
+                        <BlogDetails/>
+                    </Route>  
             </Switch>
 
         </Router>
